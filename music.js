a835a1c078ee8187ce48a2f539ed8d9c974ff0a6
Yaron Benjamin <ybenjami@gmail.com>
2021-07-15 22:26:06 -0400

    initial commit

---
+++ music.js
@@ -0,0 +1,37 @@
+
+
+exports.handler = async (event, context) => {
+  const { key } = process.env;
+  const querystring = require("querystring");
+  
+  const axios = require("axios");
+  const params = querystring.parse(event.body);
+  const searchString = params.search;
+
+  let data = '';
+
+  var options = {
+    method: 'GET',
+    url: 'https://shazam.p.rapidapi.com/search',
+    params: {term: searchString, locale: 'en-US', offset: '0', limit: '5'},
+    headers: {
+      'x-rapidapi-key': key,
+      'x-rapidapi-host': 'shazam.p.rapidapi.com'
+    }
+  };
+  
+  axios.request(options).then(function (response) {
+     data = response.data;
+  }).catch(function (error) {
+      data = error;
+  });
+
+  return {
+    statusCode: 200,
+    body: `Hello, ${name}`,
+  };
+};
+
+
+
+
