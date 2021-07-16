d9fccf2bdb64c8137f9fb122c493c0d2772425b5
Yaron Benjamin <ybenjami@gmail.com>
2021-07-16 01:25:01 -0400

    bug fixes

--- src/music.js
+++ src/music.js
@@ -3,7 +3,7 @@ exports.handler = async (event, context) => {
   const { key } = process.env;
   const querystring = require("querystring");
   const params = querystring.parse(event.body);
-  const search = require('./shazam/search.js');
+  const search = require('./shazam/search.js').search;
   const data = await search(params.search, key);
   return data;
 };
