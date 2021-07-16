d66b39dbdd9290b3684aa55074b468ee3f56a42a
Yaron Benjamin <ybenjami@gmail.com>
2021-07-16 09:31:58 -0400

    Shazam Back in

--- src/music.js
+++ src/music.js
@@ -1,14 +1,14 @@
 exports.handler = async (event, context) => {
   console.log(event);
   const { key } = process.env;
-  const params = event.queryStringParameters.search;
+  const term = event.queryStringParameters.search;
 
-  console.log(params);
+  console.log('search tearm', term);
   const shazam = require('./shazam/search');
-  const data = await shazam.search(params.search, key);
+  const response = await shazam.search(term, key);
   console.log('returned data ', data);
   return {
     statusCode: 200,
-    body: JSON.parse(data)
+    body: JSON.parse(response.data)
   };
 };
