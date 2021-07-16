4c31985111efcac5d609f1c1e95325f7a9ecf492
Yaron Benjamin <ybenjami@gmail.com>
2021-07-16 02:01:50 -0400

    adding logging

--- src/music.js
+++ src/music.js
@@ -3,8 +3,11 @@ exports.handler = async (event, context) => {
   const { key } = process.env;
   const querystring = require("querystring");
   const params = querystring.parse(event.body);
-  const shazam = require('./shazam/search.js');
+  const shazam = require('./shazam/search');
   const data = await shazam.search(params.search, key);
-  console.log('returned data ', data)
-  return data;
+  console.log('returned data ', data);
+  return {
+    statusCode: 200,
+    data
+  };
 };
